5ff5eeba977b23b1001e1a83097f0bc81c39c551
Angel David Hurtado Franco <angel.hurtado@grupoaw.co>
2022-04-08 11:57:50 -0500

    Ajustes para el ejercicio 2

--- lqn-web/src/components/pages/Exercise02/index.tsx
+++ lqn-web/src/components/pages/Exercise02/index.tsx
@@ -1,17 +1,13 @@
 import React, { useEffect, useState } from "react";
+
 import "./assets/styles.css";
 
 export default function Exercise02 () {
     const [pokemons, setPokemons] = useState(["audino", "bagon", "baltoy", "banette", "bidoof", "braviary", "bronzor", "carracosta", "charmeleon", "cresselia", "croagunk", "darmanitan", "deino", "emboar", "emolga", "exeggcute", "gabite", "girafarig", "gulpin", "haxorus", "heatmor", "heatran", "ivysaur", "jellicent", "jumpluff", "kangaskhan", "kricketune", "landorus", "ledyba", "loudred", "lumineon", "lunatone", "machamp", "magnezone", "mamoswine", "nosepass", "petilil", "pidgeotto", "pikachu", "pinsir", "poliwrath", "poochyena", "porygon2", "porygonz", "registeel", "relicanth", "remoraid", "rufflet", "sableye", "scolipede", "scrafty", "seaking", "sealeo", "silcoon", "simisear", "snivy", "snorlax", "spoink", "starly", "tirtouga", "trapinch", "treecko", "tyrogue", "vigoroth", "vulpix", "wailord", "wartortle", "whismur", "wingull", "yamask"]);
-
-    const [pokemonsSort, setPokemonsSort] = useState([]);
-
-    const endsWith = (word: string) => word[word.length - 1];
-    const getCandidates = (words: any, used: any) => words.filter((e: any) => !used.includes(e));
+    const [pokemonsList, setPokemonsList] = useState(Array);
 
     const buildLookup = (words: string[]) => {
         const lookup = new Map();
-
         words.forEach((e: string)=> {
             const start = e[0];
             lookup.set(start, [...(lookup.get(start) || []), e]);
@@ -20,52 +16,58 @@ export default function Exercise02 () {
         return lookup;
     };
 
-    const findPaths = (names: string[]) => {
-        const lookup = buildLookup(names);
+    const findPokemons = (words: string[] = pokemons) => {
+        const lookup = buildLookup(words);
         let maxNum = 0;
 
-        const parseResult = (arr: any) => {
+        const endsWith = (word: string) => word[word.length - 1];
+        const getCandidates = (words: any, used: string[]) => words.filter((e: any) => !used.includes(e));
+
+        const parseResult = (arr: Array<[]>) => {
             if (typeof arr[0] === 'object') {
-                arr.forEach((el: any) => parseResult(el))
+                arr.forEach((el: Array<[]>) => parseResult(el))
             } 
             else {
                 if (arr.length > maxNum) {
                     maxNum = arr.length;
+                    setPokemonsList([arr]);
                 }
-
+    
                 if (arr.length === maxNum) {
-                    setPokemonsSort(arr);
+                    setPokemonsList(arr);
                 }
             }
         };
-       
-        const searchWords = (word: string, res: any) => {
+
+        const searchWords = (word: string, res: string[]) => {
             const cs = getCandidates(lookup.get(endsWith(word)) || [], res);
-            return cs.length ? cs.map((e: any) => searchWords(e, [...res, e])) : res;
+            return cs.length ? cs.map((e: string) => searchWords(e, [...res, e])) : res;
         };
-       
-        names.forEach((word: string) => {
+
+        words.forEach((word: string) => {
             const res = searchWords(word, [word]);
             parseResult(res);
         });
-    };
+    }
 
     useEffect(() => {
-        findPaths(pokemons);
+        findPokemons(pokemons);
     }, [])
 
     return (
         <section className="exercise02">
             <div className="pokemons__list">
+                <h1>Lista original</h1>
                 {pokemons.map((name, i) => (
                     <p key={i}>{name}</p>
                 ))}
-            </div>            
+            </div>
             <div className="pokemons__list">
-                {pokemonsSort.map((name, i) => (
-                    <p key={i}>{name}</p>
+                <h1>Lista nueva</h1>
+                {pokemonsList.map((name, i) => (
+                    <p key={i}><>{name}</></p>
                 ))}
-            </div>  
+            </div>
         </section>
     );
 } 
